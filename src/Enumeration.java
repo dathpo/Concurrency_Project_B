87396b34b55cdcadcf668eabf6b08647f0619177
Liam Macpherson <cxb14171@ds.strath.ac.uk>
2016-10-24 10:49:21 +0100

    adding test things and fixing enumerate class

--- src/Enumeration.java
+++ src/Enumeration.java
@@ -7,15 +7,19 @@ public class Enumeration {
 	}
 	
 	public ThreadGroup[] createArrayOfGroups(){
-		Thread[] allThreads = null;
-		root.enumerate(allThreads);
+		Thread[] allThreads = new Thread[100];
+		root.getThreadGroup().enumerate(allThreads);
+		System.out.println(allThreads);
 		int j = 0;
-		for(int i = 0; allThreads.length <1; i++){
+		for(int i = 0; i<allThreads.length ; i++){
 			groups[j] = allThreads[i].getThreadGroup();
 				if(!allThreads[i].getThreadGroup().equals(allThreads[i].getThreadGroup())){
 					j++;
+					System.out.println("groups " + groups[j]);
 				}
 		}
+		
 		return groups;	
+		
 	}	
 }
--- src/main.java
+++ src/main.java
@@ -2,6 +2,14 @@
 public class main {
 	public static void main(String args[]){
 		
+		ThreadGroup g = new ThreadGroup("A");
+		Thread t1 = new Thread();
+		Thread t2 = new Thread();
+		Thread t3 = new Thread();
+		Thread t4 = new Thread();
+		
+		Enumeration e = new Enumeration(t3);
+		e.createArrayOfGroups();
 
 	}
 }
